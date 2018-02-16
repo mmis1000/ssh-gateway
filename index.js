9b58bc9c1de7c921a0057f0419524d1a8c5afc07
mmis1000 <mmis10002@gmail.com>
2018-02-16 15:54:42 +0000

    Implement ws forward and socks v5 proxy

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const config = require("./config");
-const httpServer = require("./lib/http_server")
-const sshServer = require("./lib/ssh_server")
+const httpServer = require("./lib/http_server");
+const sshServer = require("./lib/ssh_server");
+const socks5Server = require("./lib/socks5_server");
 
 httpServer(config);
-sshServer(config)
+sshServer(config);
+socks5Server(config);
